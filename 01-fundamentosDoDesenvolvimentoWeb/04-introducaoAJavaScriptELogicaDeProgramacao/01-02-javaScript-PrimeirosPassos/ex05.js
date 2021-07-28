be32f46244bc5cc350379b3648af87d64e4f357a
djsilva <coxcorp@gmail.com>
2021-07-28 19:36:02 -0300

    exercicio 05

---
+++ 01-fundamentosDoDesenvolvimentoWeb/04-introducaoAJavaScriptELogicaDeProgramacao/01-02-javaScript-PrimeirosPassos/ex05.js
@@ -0,0 +1,11 @@
+let a = 60;
+let b = 60;
+let c = -50;
+
+if (a < 0 || b < 0 || c < 0) {
+    console.log('erro');
+} else if ((a + b+ c) == 180) {
+    console.log(true);
+} else {
+    console.log(false);
+}
