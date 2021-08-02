ec290ba0ba045bc66bb33a16072fde2f932876d4
djsilva <coxcorp@gmail.com>
2021-08-02 19:06:53 -0300

    atualização de exercicios

--- 01-fundamentosDoDesenvolvimentoWeb/04-introducaoAJavaScriptELogicaDeProgramacao/03-javaScript-LogicaDeProgramacaoEAlgoritmos/ex02.js
+++ 01-fundamentosDoDesenvolvimentoWeb/04-introducaoAJavaScriptELogicaDeProgramacao/03-javaScript-LogicaDeProgramacaoEAlgoritmos/ex02.js
@@ -1,11 +1,9 @@
-let n = 3;
+let n = 5;
 let linha = '';
 let simbolo = '*';
 
 for(let i = 0; i <= n; i += 1) {
     console.log(linha);
-    for(let j = 0; j < n; j += 1) {
-        linha = linha + simbolo;
-        break
-    }
+    linha = linha + simbolo;
+
 }
